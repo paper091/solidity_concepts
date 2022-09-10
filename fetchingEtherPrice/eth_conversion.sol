e03b409fe188abb5d2d09a505ac7d771409d3019
unknown <nitishrawat009@gmail.com>
2022-09-11 00:52:18 +0530

    Added usd to eth conversion

--- fetchingEtherPrice/eth_conversion.sol
+++ fetchingEtherPrice/eth_conversion.sol
@@ -1,23 +1,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-
+// importing the chainlink interface from github using npm
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
 
 contract ethConversions{
 
     AggregatorV3Interface internal priceTicker;
     
     constructor(){
+        // initialising the AggregatorV3Interface object with the eth/usd pair contract address
         priceTicker = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
-            // 0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
     }
 
+    // this function returns the current price of eth/usd with 1e18 decimal places
     function getCurrentPrice() public view returns(uint256){
         (, int256 price, , , ) = priceTicker.latestRoundData();
-        return uint256((price*1e10)/1e18);
+        return uint256(price*1e10);
+    }
+
+    // this function returns the eth equivalent of given usd amount
+    // the eth equivalent is returned in two parts; integral part and decimal part separately
+    // the decimal part contains 18 decimal values
+    function usdToEth(uint USD) public view returns(uint256, uint256){
+        uint ethPrice = getCurrentPrice();
+        USD = USD * 1e36;
+        uint256 value = USD/ethPrice;
+        uint256 integralPart = value/1e18;
+        uint256 decimalPart = value - (integralPart*1e18);
+        return (integralPart, decimalPart);
     }
 }
-
-// Contract deployed on Goerli testnet
-// 0x4095fE4f26Df086f31b3CC44820e51c6C0EcC2D9
